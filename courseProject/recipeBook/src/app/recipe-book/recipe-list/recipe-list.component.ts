5415e62764c1091b525f8bfcb8047dcfa07c23f5
theTrackSuitDev <dimiro89@gmail.com>
2024-04-26 21:43:32 +0300

    Added Recipe model and Recipes List with dummy data

--- courseProject/recipeBook/src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ courseProject/recipeBook/src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe("Musaka", "Delicious dish with chopped meat and potatoes.", "https://matekitchen.com/wp-content/uploads/2019/08/153b_musaka_s_kartofi.jpg")
+  ];
+  
 }
